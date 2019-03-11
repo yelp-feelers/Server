bd2e1b097c8fd839fbfec11372dc4baf2a40f833
croy <roy.christie95@gmail.com>
2019-03-11 13:13:24 -0500

    Added login endpoint, now creating tokenFunctions file

--- routes/auth/authRouter.js
+++ routes/auth/authRouter.js
@@ -20,3 +20,24 @@ router.post('/signup', (req, res) => {
         );
 });
 
+// Login endpoint === `/api/auth/login`
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    // assuming a findBy() will be included in usersModel - else change name to reflect correct fn
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                const token = tokenFunctions.createToken(user);
+                res.status(200).json(token);
+            } else {
+                res.status(401).json({ msg: 'Not authorized'});
+            }
+        })
+        .catch(err =>
+            res.status(500).json(err)
+        );
+});
+
+module.exports = router;
