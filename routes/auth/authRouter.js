07e9141adeab2c54a782e3f64f68be0aa7399774
croy <roy.christie95@gmail.com>
2019-03-11 13:09:57 -0500

    Completed signup endpoint

--- routes/auth/authRouter.js
+++ routes/auth/authRouter.js
@@ -1,1 +1,22 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+// Users will call users db after its creation
+const Users = require('');
+
+// Signup endpoint === `/api/auth/signup`
+router.post('/signup', (req, res) => {
+    let user = req.bodt;
+    const hash = bcrypt.hashSync(user.password, 14);
+    user.password = hash;
+
+    Users
+        .insert(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(err => 
+            res.status(400).json(error)
+        );
+});
+
