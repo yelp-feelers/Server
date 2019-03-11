cd6910924fba401ddd16f8d4cc449f73113c3c39
WebMats <lorlando127@gmail.com>
2019-03-11 13:57:48 -0400

    initialized project

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const server = express();
+
+
+server.use(helmet())
+server.use(express.json())
+server.use(express.urlencoded({ extended: false }));
+
+//==============================================================// CORS setup
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Headers', "Origin, Accept, Content-Type, Authorization, X-Requested-With");
+    res.setHeader('Access-Control-Allow-Methods', "POST,GET,OPTIONS,DELETE,PUT");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+})
+//==============================================================// ROUTES
+
+const PORT = process.env.PORT || '4200';
+server.listen(PORT, () => {console.log(`Listening on port ${PORT}...`)})
