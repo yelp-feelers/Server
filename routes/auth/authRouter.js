4936c9e72dc25c316e6f35f5cc8aacfd1fed3c4c
WebMats <lorlando127@gmail.com>
2019-03-11 16:43:00 -0400

    signup and login endpoints have basic functionality

--- routes/auth/authRouter.js
+++ routes/auth/authRouter.js
@@ -1,37 +1,52 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
-const tokenFunctions = require('./tokenFunctions.js');
+const { createToken } = require('./tokenFunctions.js');
 const Users = require('../../data/helpers/usersModel');
 
 // Signup endpoint === `/api/auth/signup`
-router.post('/signup', (req, res) => {
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 14);
-    user.password = hash;
-
-    Users
-        .addReviewer(user)
-        .then(saved => {
-            res.status(201).json(saved);
-        })
-        .catch(err => 
-            res.status(400).json(err)
-        );
+router.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(404).json({errorMessage: "Please provide a valid email and password."})
+    }
+    const userFound = await Users.getReviewerByUserName(username);
+    if (userFound) {
+        return res.status(404).json({errorMessage: "Username is taken."})
+    }
+    try {
+        const hash = bcrypt.hashSync(password, 12);
+        if (!hash) {
+            res.status(404).json({errorMessage: "Could not create user"})
+        } else {
+            const newUser = {username, hash}
+            Users.addReviewer(newUser).then(saved => {
+                const token = createToken(saved);
+                const userToSend = {...saved, token, tokenExpiration: 1440}
+                res.status(201).json(userToSend);
+            }).catch((err) => {
+                console.log(err)
+            });;
+        }
+    } catch (error) {
+        console.log(err)
+    }
 });
 
 // Login endpoint === `/api/auth/login`
 router.post('/login', (req, res) => {
-    let { username, password } = req.body;
-
-    Users.getReviewerByUser({ username })
-        .first()
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(404).json({errorMessage: "Please provide a valid email and password."})
+    }
+    Users.getReviewerByUserName(username)
         .then(user => {
-            if(user && bcrypt.compareSync(password, user.password)) {
-                const token = tokenFunctions.createToken(user);
-                res.status(200).json(token);
+            if(user && bcrypt.compareSync(password, user.hash)) {
+                const token = createToken(user);
+                const userToSend = {id: user.id, username: user.username, token, tokenExpiration: 1440}
+                res.status(200).json(userToSend);
             } else {
-                res.status(401).json({ msg: 'Not authorized'});
+                res.status(401).json({ errorMessage: 'Not authorized'});
             }
         })
         .catch(err =>
