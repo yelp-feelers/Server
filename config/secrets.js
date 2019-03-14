a603d5544927d42fdd50db9986feb29bc6e65a91
croy <roy.christie95@gmail.com>
2019-03-14 15:36:20 -0500

    Reverted db changes for front-end

--- config/secrets.js
+++ config/secrets.js
@@ -2,4 +2,4 @@ module.exports = {
     jwtSecret: process.env.JWT_SECRET || 'One of them should have seen it coming',
     DATABASE_URL: process.env.DATABASE_URL,
     BUSINESS_DATABASE_URL: process.env.DATABASE_URL,
-}
+}  
