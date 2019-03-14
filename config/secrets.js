dbc751c3281e43c72647ec0141bb674d871bb4b2
croy <roy.christie95@gmail.com>
2019-03-14 14:46:29 -0500

    Added exports to secrets.js

--- config/secrets.js
+++ config/secrets.js
@@ -1,3 +1,5 @@
 module.exports = {
-    jwtSecret: process.env.JWT_SECRET || 'One of them should have seen it coming'
+    jwtSecret: process.env.JWT_SECRET || 'One of them should have seen it coming',
+    DATABASE_URL: process.env.DATABASE_URL,
+    BUSINESS_DATABASE_URL: process.env.DATABASE_URL,
 }
